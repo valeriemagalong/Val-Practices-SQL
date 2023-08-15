3c789badeea6fa2bd0a9de0d976ca0075d824698
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-15 18:26:31 -0400

    Create users_third_transaction.sql and add question from DataLemur

---
+++ Users_Third_Transaction/users_third_transaction.sql
@@ -0,0 +1,9 @@
+/*
+Question sourced from DataLemur.com
+https://datalemur.com/questions/sql-third-transaction
+
+Database: PostgreSQL
+*/
+
+-- Assume you are given the table below on Uber transactions made by users.  Write a query to
+-- obtain the third transaction of every user. Output the user id, spend and transaction date.
