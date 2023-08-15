2f511effdb7393f6bb1779173089489b6d7b655f
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-15 18:41:41 -0400

    Add answer to users_third_transaction.sql

--- Users_Third_Transaction/users_third_transaction.sql
+++ Users_Third_Transaction/users_third_transaction.sql
@@ -5,5 +5,14 @@ https://datalemur.com/questions/sql-third-transaction
 Database: PostgreSQL
 */
 
--- Assume you are given the table below on Uber transactions made by users.  Write a query to
+-- Assume you are given a table on Uber transactions made by users.  Write a query to
 -- obtain the third transaction of every user. Output the user id, spend and transaction date.
+
+WITH transactions_ranked AS (
+  SELECT *,
+    RANK() OVER (PARTITION BY user_id ORDER BY transaction_date) AS transaction_number
+  FROM transactions
+)
+SELECT user_id, spend, transaction_date
+FROM transactions_ranked
+WHERE transaction_number = 3;
