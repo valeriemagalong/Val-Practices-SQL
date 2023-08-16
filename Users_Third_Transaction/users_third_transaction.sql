5fbfc900e18398c312e4335cc941066440fb25fb
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-15 21:38:33 -0400

    Add comment about problem solving strategy to users_third_transaction.sql

--- Users_Third_Transaction/users_third_transaction.sql
+++ Users_Third_Transaction/users_third_transaction.sql
@@ -8,6 +8,9 @@ Database: PostgreSQL
 -- Assume you are given a table on Uber transactions made by users.  Write a query to
 -- obtain the third transaction of every user. Output the user id, spend and transaction date.
 
+-- My strategy: Derive a column that has the rank of a given transaction for a specific user,
+-- then filter rows by transactions that are ranked third.
+
 WITH transactions_ranked AS (
   SELECT *,
     RANK() OVER (PARTITION BY user_id ORDER BY transaction_date) AS transaction_number
