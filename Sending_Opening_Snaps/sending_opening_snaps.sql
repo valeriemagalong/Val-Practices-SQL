c08930eed7d8a4aa4e7407ac91c2de590f4acfd5
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-16 17:12:31 -0400

    Create sending_opening_snaps.sql and add question from DataLemur

---
+++ Sending_Opening_Snaps/sending_opening_snaps.sql
@@ -0,0 +1,13 @@
+/*
+Question sourced from DataLemur.com
+https://datalemur.com/questions/time-spent-snaps
+
+Database: PostgreSQL
+*/
+
+-- Assume you're given tables with information on Snapchat users, including
+-- their ages and time spent sending and opening snaps.
+
+-- Write a query to obtain a breakdown of the time spent sending vs. opening
+-- snaps as a percentage of total time spent on these activities grouped by age
+-- group. Round the percentage to 2 decimal places in the output.
