4d7ed5d48b8820c740a7ac8aa6c716a7acdf10b5
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-17 18:24:06 -0400

    Add solution to Tweets' Rolling Average question to .sql file

--- Tweets_Rolling_Averages/tweets_rolling_averages.sql
+++ Tweets_Rolling_Averages/tweets_rolling_averages.sql
@@ -9,6 +9,17 @@ Database: PostgreSQL
 -- average of tweets for each user. Output the user ID, tweet date, and rolling averages
 -- rounded to 2 decimal places.
 
--- My strategy: Per Twitter user, for each row of data, i.e. a tweet
--- date, calculate the average tweet count for the row and its preceding
--- two rows (using a window function).
+-- My strategy: Per Twitter user, for each row of data (i.e. a tweet
+-- date), calculate the average tweet count for the row and its preceding
+-- two rows (current day and two days before).
+
+-- Window function syntax to remember: ROWS clause can be using to specify the rows
+-- that the window function acts over (i.e. ROWS BETWEEN lower AND upper bound).
+
+SELECT user_id, tweet_date,
+  ROUND(AVG(tweet_count) OVER (
+    PARTITION BY user_id
+    ORDER BY tweet_date
+    ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ), 2) AS rolling_avg_3d
+FROM tweets
