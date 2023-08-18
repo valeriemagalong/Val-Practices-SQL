63477c870ac0ee02ee0072511e55c9b096e2fec6
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-18 18:03:52 -0400

    Create highest_grossing_items.sql and add challenge question

---
+++ Highest_Grossing_Items/highest_grossing_items.sql
@@ -0,0 +1,12 @@
+/*
+Question sourced from DataLemur.com
+https://datalemur.com/questions/sql-highest-grossing
+
+Database: PostgreSQL
+*/
+
+-- Assume you're given a table containing data on Amazon customers and their
+-- spending on products in different category, write a query to identify
+-- the top two highest-grossing products within each category in the year
+-- 2022. The output should include the category, product, and total spend.
+
