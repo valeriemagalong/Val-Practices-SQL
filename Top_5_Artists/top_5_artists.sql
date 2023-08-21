da7a6d11e319b365914be2d8a051863c781c23c7
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-21 19:08:39 -0400

    Write solution to Top 5 Artists challenge in top_5_artists.sql

--- Top_5_Artists/top_5_artists.sql
+++ Top_5_Artists/top_5_artists.sql
@@ -20,3 +20,22 @@ Database: PostgreSQL
     -- For instance, if both Ed Sheeran and Bad Bunny appear in the Top 10 five
     -- times, they should both be ranked 1st and the next artist should be ranked 2nd.
 
+-- My strategy: Join the artists, songs, and global_song_rank tables,
+-- filtering by rows with a rank of 1 through 10, grouping by artist,
+-- aggregating by song count, and using DENSE_RANK to get the artist rank
+-- by song count.  Wrap this query in a CTE, and output the top 5 artists.
+
+WITH top_artists AS (
+  SELECT a.artist_name, COUNT(gsr.song_id) as total_songs,
+    DENSE_RANK() OVER(
+      ORDER BY COUNT(gsr.song_id) DESC
+    ) AS artist_rank
+  FROM artists a
+  JOIN songs s ON a.artist_id = s.artist_id
+  JOIN global_song_rank gsr ON s.song_id = gsr.song_id
+  WHERE gsr.rank BETWEEN 1 AND 10
+  GROUP BY a.artist_name
+)
+SELECT artist_name, artist_rank
+FROM top_artists
+WHERE artist_rank BETWEEN 1 AND 5;
