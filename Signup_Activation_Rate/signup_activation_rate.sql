141508a794a446958c1b74fb139f94f0d3204dd3
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-22 19:49:10 -0400

    Add strategy comment and solution to signup_activation_rate.sql

--- Signup_Activation_Rate/signup_activation_rate.sql
+++ Signup_Activation_Rate/signup_activation_rate.sql
@@ -14,3 +14,21 @@ Database: PostgreSQL
 -- the emails table. Write a query to find the activation rate. Round the percentage
 -- to 2 decimal places.
 
+-- My strategy: Activation rate is the number of users that confirmed divided by total
+-- users.  Create a CTE for total users from the emails table.  Create a CTE for
+-- number of confirmed users from the texts table.  Finally, derive activation rate
+-- from these two CTEs.
+
+WITH all_users AS (
+  SELECT COUNT(user_id) AS total_users
+  FROM emails
+),
+all_confirmed AS (
+  SELECT email_id, COUNT(*) AS num_confirms
+  FROM texts
+  WHERE signup_action = 'Confirmed'
+  GROUP BY email_id
+)
+SELECT ROUND(SUM(num_confirms) / (SELECT total_users FROM all_users), 2)
+  AS confirm_rate
+FROM all_confirmed;
