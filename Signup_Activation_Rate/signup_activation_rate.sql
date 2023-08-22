396170d992aa8ce80ebf12690b21cd20f12ce3ee
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-22 18:39:10 -0400

    Create signup_activation_rate.sql and add challenge question

---
+++ Signup_Activation_Rate/signup_activation_rate.sql
@@ -0,0 +1,16 @@
+/*
+Question sourced from DataLemur.com
+https://datalemur.com/questions/signup-confirmation-rate
+
+Database: PostgreSQL
+*/
+
+-- New TikTok users sign up with their emails. They confirmed their signup by
+-- replying to the text confirmation to activate their accounts. Users may receive
+-- multiple text messages for account confirmation until they have confirmed their
+-- new account.
+
+-- A senior analyst is interested to know the activation rate of specified users in
+-- the emails table. Write a query to find the activation rate. Round the percentage
+-- to 2 decimal places.
+
