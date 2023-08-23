0fc4379f3fa3a8433cf15f1438f705b3d6cea8cc
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-23 17:15:40 -0400

    Create supercloud_customer.sql and add challenge question

---
+++ Supercloud_Customer/supercloud_customer.sql
@@ -0,0 +1,12 @@
+/*
+Question sourced from DataLemur.com
+https://datalemur.com/questions/supercloud-customer
+
+Database: PostgreSQL
+*/
+
+-- A Microsoft Azure Supercloud customer is defined as a company that purchases at
+-- least one product from each product category.
+
+-- Write a query that effectively identifies the company ID of such Supercloud customers.
+
