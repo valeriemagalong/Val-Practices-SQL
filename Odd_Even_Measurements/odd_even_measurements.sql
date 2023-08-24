97861ba9c654957f006ae07f0fb98c23c0ee7609
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-08-24 19:11:44 -0400

    Create odd_even_measurements.sql and add challenge question

---
+++ Odd_Even_Measurements/odd_even_measurements.sql
@@ -0,0 +1,12 @@
+/*
+Question sourced from DataLemur.com
+https://datalemur.com/questions/odd-even-measurements
+
+Database: PostgreSQL
+*/
+
+-- Assume you're given a table with measurement values obtained from a Google sensor over
+-- multiple days with measurements taken multiple times within each day.
+
+-- Write a query to calculate the sum of odd-numbered and even-numbered measurements
+-- separately for a particular day and display the results in two different columns.
