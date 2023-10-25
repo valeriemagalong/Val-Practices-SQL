f07cb8884f364f5f6bd0613da5ca5b7cb23d20ba
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-10-24 22:14:25 -0400

    Create folder for Y-On-Y Growth Rate challenge and add .sql file with question

---
+++ Y_On_Y_Growth_Rate/y_on_y_growth_rate.sql
@@ -0,0 +1,18 @@
+/*
+Question sourced from DataLemur.com
+https://datalemur.com/questions/yoy-growth-rate
+
+Database: PostgreSQL
+*/
+
+-- Assume you're given a table containing information about Wayfair user transactions for
+-- different products. Write a query to calculate the year-on-year growth rate for the total
+-- spend of each product, grouping the results by product ID.
+
+-- The output should include the year in ascending order, product ID, current year's spend,
+-- previous year's spend and year-on-year growth percentage, rounded to 2 decimal places.
+
+-- My strategy:
+
+
+
