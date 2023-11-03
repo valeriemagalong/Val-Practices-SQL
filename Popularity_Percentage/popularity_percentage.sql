759ad4fd11ec228e744e449063910eb674a72292
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-11-03 19:42:58 -0400

    Create popularity_percentage.sql file and add Popularity Percentage challenge link and question

---
+++ Popularity_Percentage/popularity_percentage.sql
@@ -0,0 +1,16 @@
+/*
+Question sourced from StrataScratch.com
+https://platform.stratascratch.com/coding/10284-popularity-percentage
+
+Database: MySQL
+*/
+
+-- Find the popularity percentage for each user on Meta/Facebook. The popularity percentage is defined
+-- as the total number of friends the user has divided by the total number of users on the platform,
+-- then converted into a percentage by multiplying by 100.
+
+-- Output each user along with their popularity percentage. Order records in ascending order by user id.
+-- The 'user1' and 'user2' column are pairs of friends.
+
+-- My strategy:
+
