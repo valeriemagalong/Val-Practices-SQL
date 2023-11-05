0e0c7ca94b89af9b46578a67957e968907ba1230
Valerie Magalong <64289168+valeriemagalong@users.noreply.github.com>
2023-11-05 12:25:41 -0500

    Create .sql file with Counting Instances In Text challenge question, and add tables.png into challenge folder

---
+++ Counting_Instances_In_Text/counting_instances_in_text.sql
@@ -0,0 +1,14 @@
+/*
+Question sourced from StrataScratch.com
+https://platform.stratascratch.com/coding/9814-counting-instances-in-text
+
+Database: MySQL
+*/
+
+-- Find the number of times the words 'bull' and 'bear' occur in the contents. We're counting the number
+-- of times the words occur so words like 'bullish' should not be included in our count.
+
+-- Output the word 'bull' and 'bear' along with the corresponding number of occurrences.
+
+-- My strategy:
+
